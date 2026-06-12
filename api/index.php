d3f366a881e89e8f726b07284e91312ca97d1cc3
Dhnnz12 <dhonanhibrizi123@gmail.com>
2026-06-12 21:02:17 +0700

    .

--- api/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Mengarahkan Vercel untuk membaca autoload dan app Laravel melalui folder public
-require __DIR__ . '/../public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,23 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+// 1. Trik runtime check untuk membuat folder cache di /tmp Vercel
+if (isset($_SERVER['VERCEL_URL'])) {
+    $targetDirectories = [
+        'bootstrap/cache',
+        'storage/framework/sessions',
+        'storage/framework/views',
+        'storage/framework/cache'
+    ];
+    foreach ($targetDirectories as $dir) {
+        if (!is_dir('/tmp/' . $dir)) {
+            mkdir('/tmp/' . $dir, 0755, true);
+        }
+    }
+}
+
+// 2. Tampung konfigurasi ke dalam variabel $app
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -24,3 +40,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// 3. Alihkan storage path Laravel ke /tmp khusus di lingkungan Vercel
+if (isset($_SERVER['VERCEL_URL'])) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+// 4. Kembalikan instance $app agar kernel Laravel tetap berjalan normal
+return $app;
